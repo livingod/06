e2ec02ce5b33b636133e905031517373ea9b3665
livingod <144990631+livingod@users.noreply.github.com>
2023-10-12 16:15:46 +0900

    03

--- main.c
+++ main.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void func1(void)
+int sumTwo(int a, int b)
 {
-     int x;
-     
-     printf("func1 x is at %p\n", &x);
-     //%p << 16jinsu, &x = juso?? mola
+    int result;
+    result = a+b;
+    return result;
+    
+    //or return (a+b);
+}
+
+int square(int n)
+{
+    int result;
+    result = n*n;
+    return result;
+}
+
+int get_max(int x, int y)
+{
+    if (x>y)
+           return x;
+    else
+        return y; 
 }
 
 int main(int argc, char *argv[])
 {
-  int x;
-  
-  printf("main x is at %p\n",&x);
-  func1();
-  
+    
+    int a,b;
+    
+    a = 3;
+    b = 7;
+    
+    printf("sumTwo result is %i\n",sumTwo(a,b));
+    printf("square result is %i\n",square(a));
+    printf("get_max result is %i\n",get_max(a,b));
   
   system("PAUSE");	
   return 0;
